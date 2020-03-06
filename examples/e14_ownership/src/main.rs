f6e24b57d680adea696abd2fa680c11c386cf90d
Aydogan Ersoz <aydoganersoz@protonmail.com>
2020-03-06 08:42:24 +0100

    e14 continue

--- examples/e14_ownership/src/main.rs
+++ examples/e14_ownership/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     }
     // println!("\t{}", hello_str); // not allowed as variable is out of scope
 
-    println!("2. String assign");
+    println!("2. String type");
     {
         let s1 = String::from("hey");
         // only pointer is copied not the actual data
@@ -17,16 +17,7 @@ fn main() {
         println!("\t{}", s2);
     }
 
-    println!("3. String clone");
-    {
-        let s1 = String::from("ay");
-        // actual data is also copied
-        let s2 = s1.clone();
-        println!("\t{}", s1);
-        println!("\t{}", s2);
-    }
-
-    println!("4. String lateral");
+    println!("3. String lateral");
     {
         let s1 = "oy";
         // actual data is also copied
@@ -35,72 +26,69 @@ fn main() {
         println!("\t{}", s2);
     }
 
+    println!("4. String clone");
+    {
+        let s1 = String::from("ay");
+        // actual data is also copied
+        let s2 = s1.clone();
+        println!("\t{}", s1);
+        println!("\t{}", s2);
+    }
+
     println!("5. String borrowed by function");
     {
-        let s1 = String::from("hello world");
-        println!("\t{}", s1);
-        // function doesn't take the ownership of s1 (borrowing)
-        let _ = func_ref(&s1);
-        println!("\t{}", s1); // s1 is still in scope
+        let s = String::from("hello world");
+        println!("\t{}", s);
+        // function doesn't take the ownership of s (borrowing)
+        let _ = func_ref(&s);
+        println!("\t{}", s); // s1 is still the owner
     }
 
     println!("6. String owned by function");
     {
-        let s1 = String::from("hello world");
-        println!("\t{}", s1);
-        // function takes the ownership of s1 (owning)
-        let _ = func_str_own(s1);
-        // println!("\t{}", s1); // s1 is no longer in scope
+        let s = String::from("hello world");
+        println!("\t{}", s);
+        // function takes the ownership of s (owning)
+        let _ = func_str_own(s);
+        // println!("\t{}", s1); // s is no longer the owner
     }
 
     println!("7. Mutable string borrowed by function");
     {
-        let mut s1 = String::from("hello world");
-        println!("\t{}", s1);
-        // function doesn't take the ownership of s1 (borrowing)
-        func_mut_ref(&mut s1);
-        println!("\t{}", s1); // s1 is still in scope
+        let mut s = String::from("hello world");
+        println!("\t{}", s);
+        // function doesn't take the ownership of s (borrowing)
+        func_mut_ref(&mut s);
+        println!("\t{}", s); // s is still the owner
     }
 
-    println!("8. Multiple mutable reference");
+    println!("8. Mutable immutable references");
     {
-        // only one mutable reference is allowed at a time
+        // having immutable references while having a mutable one
+        // is allowed
         let mut s = String::from("hello rust");
         {
             let r1 = &mut s; // r1 takes the ownership of s
+            let r2 = &r1;
+            let r3 = &r1;
 
             // println!("\t{}", s); // not allowed as s has no longer the ownership
             println!("\t{}", r1);
-            // r1 releases the ownership of s
+            println!("\t{}", r2);
+            println!("\t{}", r3);
         }
         println!("\t{}", s); // s is the owner of itself again
-        let r2 = &mut s; // r2 takes the ownership of s
-
-        // println!("\t{}", s); // not allowed as s has no longer the ownership
-        println!("\t{}", r2);
     }
 
     println!("9. Multiple mutable and immutable reference");
     {
-        // we can't have a mutable reference while we have an immutable one
+        // having a mutable reference while having an immutable one
+        // is not allowed
         let mut s = String::from("bye");
 
-        // multiple immutable references are ok
         let r1 = &s;
-        // multiple immutable references are ok
         let r2 = &s;
         //let r3 = &mut s; // not allowed
-        println!("\t{}, {}", r1, r2);
-    }
-
-    println!("10. Multiple mutable and immutable reference - 2");
-    {
-        let mut s = String::from("bye");
-
-        // multiple immutable references are ok
-        let r1 = &s;
-        // multiple immutable references are ok
-        let r2 = &s;
         println!("\t{}, {}", r1, r2);
         let r3 = &mut s; // allowed as r1 and r2 are no longer in scope
         println!("\t{}", r3);
