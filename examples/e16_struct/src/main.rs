d98bca317bfa066b3fa8395377b215b2b115e979
Aydogan Ersoz <aydoganersoz@protonmail.com>
2020-03-10 08:32:55 +0100

    e16 about to finish

--- examples/e16_struct/src/main.rs
+++ examples/e16_struct/src/main.rs
@@ -1,10 +1,31 @@
 #[derive(Debug)]
 struct Student {
     id: u32,
+    name: String,
+}
+
+impl Student {
+    fn add_surname(&mut self, surname: &str) -> usize {
+        self.name.push_str(surname);
+        self.name.len()
+    }
 }
 
 fn main() {
-    let student1 = Student { id: 12_345_667 };
+    let mut student1 = Student {
+        id: 12_345_667,
+        name: String::from("jane"),
+    };
 
-    println!("\t{:?}", student1);
+    print_student_info(&student1);
+
+    let jane_surname = " brown";
+    let ret = student1.add_surname(&jane_surname);
+    println!("\t{:?}", ret);
+    print_student_info(&student1);
+}
+
+fn print_student_info(student: &Student) {
+    println!("\tstudent id: {}", student.id);
+    println!("\tstudent name: {}", student.name);
 }
