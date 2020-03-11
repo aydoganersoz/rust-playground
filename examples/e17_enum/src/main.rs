6758cd8074edf3175653c525ea4343982eed0bed
Aydogan Ersoz <aydoganersoz@protonmail.com>
2020-03-11 08:41:13 +0100

    e17 started

--- examples/e17_enum/src/main.rs
+++ examples/e17_enum/src/main.rs
@@ -1,3 +1,29 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Vegetables {
+    Cabbage,
+    Lettuce,
+    Cucumber,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum FruitCodes {
+    Strawberry(u8),
+    Pear(String),
+    Orange(u16),
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("1. Integer enum");
+    let cab = Vegetables::Cabbage;
+    println!("\t{:?}", cab);
+
+    println!("2. Enum with value");
+    let straw = FruitCodes::Strawberry(1);
+    let pear = FruitCodes::Pear(String::from("2"));
+    let orange = FruitCodes::Orange(3);
+    println!("\t{:?}", straw);
+    println!("\t{:?}", pear);
+    println!("\t{:?}", orange);
 }
