976607ce5713f08a02182e5caa3e6278d04d1d66
Aydogan Ersoz <aydoganersoz@protonmail.com>
2020-03-16 08:21:35 +0100

    e19 finished

--- examples/e19_crate/src/main.rs
+++ examples/e19_crate/src/main.rs
@@ -1,3 +1,16 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("1. Crate");
+
+    let mut rng = rand::thread_rng();
+
+    let r1: u8 = rng.gen();
+    let r2: u16 = rng.gen();
+    println!("\tu8:{}", r1);
+    println!("\tu16:{}", r2);
+    println!("\tbool:{}", rng.gen::<bool>());
+    println!("\tu32:{}", rng.gen::<u32>());
+    println!("\ti32:{}", rng.gen::<i32>());
+    println!("\tfloat:{}", rng.gen::<f64>());
 }
