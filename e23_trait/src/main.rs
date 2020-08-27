1c4a89e725eeb330317f9ffce3ec53ed85e1741d
Aydogan Ersoz <aydoganersoz@protonmail.com>
2020-08-27 17:58:14 +0200

    e23 trait finished

--- e23_trait/src/main.rs
+++ e23_trait/src/main.rs
@@ -1,3 +1,14 @@
+mod clock;
+
 fn main() {
-    println!("Trait example");
+    let my_clock = clock::Clock {
+        hours: 23,
+        minutes: 59,
+    };
+
+    println!("1. ToString trait");
+    println!("my_clock.to_string() = {:?}", my_clock.to_string());
+
+    println!("2. From trait");
+    println!("String::from(my_clock) = {:?}", String::from(my_clock));
 }
