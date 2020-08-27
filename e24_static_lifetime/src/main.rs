ac3258089b72d935b0e4792450f6abfc84a84a48
Aydogan Ersoz <aydoganersoz@protonmail.com>
2020-08-27 19:23:32 +0200

    e24 static lifetime finished

---
+++ e24_static_lifetime/src/main.rs
@@ -0,0 +1,33 @@
+static MY_STATIC_I32: i32 = 42;
+static MY_STATIC_STR: &'static str = "hey!";
+
+fn main() {
+    println!("1. Return static types");
+
+    // these variables will live for the entire lifetime of the program
+    let retval_1 = return_static_string();
+    let retval_2 = return_static_i32();
+    let retval_3 = return_static_string_2();
+    let retval_4 = argument_static_i32(&MY_STATIC_I32);
+
+    println!("{:?}", retval_1);
+    println!("{:?}", retval_2);
+    println!("{:?}", retval_3);
+    println!("{:?}", retval_4);
+}
+
+fn return_static_string() -> &'static str {
+    "yay!"
+}
+
+fn return_static_string_2() -> &'static str {
+    MY_STATIC_STR
+}
+
+fn return_static_i32() -> &'static i32 {
+    &MY_STATIC_I32
+}
+
+fn argument_static_i32(arg: &'static i32) -> i32 {
+    arg + 1
+}
