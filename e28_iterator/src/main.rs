7d93ffeee4b9aa33451a20a79134d612f6603280
Aydogan Ersoz <aydoganersoz@protonmail.com>
2020-11-24 08:46:33 +0100

    terminated iterator examples

--- e28_iterator/src/main.rs
+++ e28_iterator/src/main.rs
@@ -46,11 +46,6 @@ fn main() {
     let arr = [1, 2, 3, 4, 5];
     println!("\t{:?}", arr.iter().count());
 
-    println!("last");
-    // consumes the iterator
-    let arr = [1, 2, 3, 4, 5, 6];
-    println!("\t{:?}", arr.iter().last().unwrap());
-
     println!("cycle");
     let arr = [1, 2, 3, 4, 5];
     let mut cnt = 0;
@@ -67,15 +62,6 @@ fn main() {
     for (idx, val) in arr.iter().enumerate() {
         println!("\t({:?}, {:?})", idx, val);
     }
-
-    println!("eq");
-    let arr = ['a', 'b', 'z', 'w'];
-    let arr2 = ['b', 'a', 'z', 'w'];
-    let arr3 = [1, 2, 3];
-    let arr4 = [1, 2, 3];
-    println!("\t{:?}", arr.iter().eq(arr2.iter()));
-    println!("\t{:?}", arr2.iter().eq(arr.iter()));
-    println!("\t{:?}", arr3.iter().eq(arr4.iter()));
 
     println!("filter");
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 13];
@@ -97,6 +83,139 @@ fn main() {
     let numbers = [2, 4, 6, 9, 12];
     let folded = numbers.iter().fold(200, |acc, x| x + acc);
     println!("\t{:?}", folded);
+
+    println!("eq");
+    let arr1 = ['a', 'b', 'c'];
+    let arr2 = ['a', 'b', 'c'];
+    let arr3 = ['a', 'b'];
+    println!("\t{:?}", arr1.iter().eq(arr2.iter()));
+    println!("\t{:?}", arr1.iter().eq(arr3.iter()));
+
+    println!("ge");
+    let arr1 = ['a', 'b', 'c'];
+    let arr2 = ['a', 'b', 'c'];
+    let arr3 = ['a', 'b'];
+    println!("\t{:?}", arr1.iter().ge(arr2.iter()));
+    println!("\t{:?}", arr1.iter().ge(arr3.iter()));
+
+    println!("gt");
+    let arr1 = ['a', 'b', 'c'];
+    let arr2 = ['a', 'b', 'c'];
+    let arr3 = ['a', 'b'];
+    println!("\t{:?}", arr1.iter().gt(arr2.iter()));
+    println!("\t{:?}", arr1.iter().gt(arr3.iter()));
+
+    println!("le");
+    let arr1 = ['a', 'b', 'c'];
+    let arr2 = ['a', 'b', 'c'];
+    let arr3 = ['a', 'b'];
+    println!("\t{:?}", arr1.iter().le(arr2.iter()));
+    println!("\t{:?}", arr1.iter().le(arr3.iter()));
+
+    println!("lt");
+    let arr1 = ['a', 'b', 'c'];
+    let arr2 = ['a', 'b', 'c'];
+    let arr3 = ['a', 'b'];
+    println!("\t{:?}", arr1.iter().lt(arr2.iter()));
+    println!("\t{:?}", arr1.iter().lt(arr3.iter()));
+
+    println!("ne");
+    let arr1 = ['a', 'b', 'c'];
+    let arr2 = ['a', 'b', 'c'];
+    let arr3 = ['a', 'b'];
+    println!("\t{:?}", arr1.iter().ne(arr2.iter()));
+    println!("\t{:?}", arr1.iter().ne(arr3.iter()));
+
+    println!("last");
+    // consumes the iterator
+    let arr = [1, 2, 3, 4, 5, 6];
+    println!("\t{:?}", arr.iter().last().unwrap());
+
+    println!("nth");
+    // consumes the iterator up to the nth element
+    let arr = [1, 2, 3, 4, 5, 6];
+    println!("\t{:?}", arr.iter().nth(3).unwrap());
+
+    println!("map");
+    let arr = [1, 2, 3, 4, 5, 6];
+    let mapped: Vec<i32> = arr.iter().map(|x| x * 2).collect();
+    println!("\t{:?}", mapped);
+
+    println!("max");
+    let arr = [1, 2, 3, 4, 5, 6];
+    println!("\t{:?}", arr.iter().max().unwrap());
+
+    println!("min");
+    let arr = [1, 2, 3, 4, 5, 6];
+    println!("\t{:?}", arr.iter().min().unwrap());
+
+    println!("position");
+    let arr = ["p", "h", "r", "t"];
+    println!("\t{:?}", arr.iter().position(|&x| x == "r"));
+
+    println!("rposition");
+    let arr = ["p", "h", "r", "t"];
+    println!("\t{:?}", arr.iter().rposition(|&x| x == "r"));
+
+    println!("product");
+    let arr = [1, 2, 3, 4, 5, 6];
+    println!("\t{:?}", arr.iter().product::<i32>());
+
+    println!("rev");
+    let arr = [1, 2, 3, 4, 5, 6];
+    let arrit = arr.iter().rev();
+    for i in arrit {
+        println!("\ti = {:?}", i);
+    }
+
+    println!("skip");
+    let arr = [1, 2, 3, 4, 5, 6];
+    let arrit = arr.iter().skip(2);
+    for i in arrit {
+        println!("\ti = {:?}", i);
+    }
+
+    println!("step_by");
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    let arrit = arr.iter().step_by(2);
+    for i in arrit {
+        println!("\ti = {:?}", i);
+    }
+
+    println!("sum");
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    println!("\t{:?}", arr.iter().sum::<i32>());
+
+    println!("take");
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    let arrit = arr.iter().take(10);
+    for i in arrit {
+        println!("\ti = {:?}", i);
+    }
+
+    println!("zip");
+    let it1 = [1, 2, 3];
+    let it2 = [4, 5, 6];
+    let it1it2 = it1.iter().zip(it2.iter());
+    for (i, j) in it1it2 {
+        println!("\t(i, j) = ({:?}, {:?})", i, j);
+    }
+    let it1it2 = it1.iter().zip(it2.iter());
+    for tup in it1it2 {
+        println!("\ttup = {:?}", tup);
+    }
+
+    println!("unzip");
+    let it1 = [1, 2, 3];
+    let it2 = [4, 5, 6];
+    let it1it2 = it1.iter().zip(it2.iter());
+    let (left, right): (Vec<i32>, Vec<i32>) = it1it2.unzip();
+    for i in left {
+        println!("\tit1(i) = {:?}", i);
+    }
+    for i in right {
+        println!("\tit2(i) = {:?}", i);
+    }
 
     // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.ge
 }
