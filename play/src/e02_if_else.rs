107d4fcba266684ab79ee12429d5533c6f757827
Aydogan Ersoz <aydoganersoz@protonmail.com>
2021-03-09 19:35:05 +0100

    progress

---
+++ play/src/e02_if_else.rs
@@ -0,0 +1,31 @@
+fn test1() {
+    let temperature = 23;
+    let weather;
+
+    if temperature < 21 {
+        weather = "cold weather"
+    } else if temperature >= 21 && temperature < 31 {
+        weather = "mild weather"
+    } else {
+        weather = "hot weather"
+    }
+
+    assert_eq!(weather, "mild weather");
+}
+
+fn test2() {
+    let temperature = 23;
+
+    let weather = if temperature >= 28 {
+        "hot weather"
+    } else {
+        "cold weather"
+    };
+
+    assert_eq!(weather, "cold weather");
+}
+
+pub fn test() {
+    test1();
+    test2();
+}
---
+++ play/src/main.rs
@@ -0,0 +1,5 @@
+mod e02_if_else;
+
+fn main() {
+    e02_if_else::test();
+}
