b99e878be5a0100227ca1e0a1f449948a5348af6
Aydogan Ersoz <aydoganersoz@protonmail.com>
2021-03-17 08:19:08 +0100

    refactor functions

--- src/e12_functions.rs
+++ src/e12_functions.rs
@@ -1,32 +1,6 @@
-// no argument no return"
+// no argument no return
 fn test1() {
     func01();
-
-    println!("4. Two integer argument integer return");
-    let ret = func04(5, -3);
-    println!("\tret: {}", ret);
-
-    println!("5. Two integer argument integer return with `return`");
-    let ret = func05(5, -3);
-    println!("\tret: {}", ret);
-
-    println!("6. String reference argument");
-    let s = String::from("hello world");
-    println!("\t{}", s);
-    let ret = func06(&s);
-    println!("\t{}", ret);
-
-    println!("7. String argument");
-    let s = String::from("hello world");
-    println!("\t{}", s);
-    let ret = func07(s);
-    println!("\t{}", ret);
-
-    println!("8. Mutable string reference argument");
-    let mut s = String::from("hello world");
-    println!("\t{}", s);
-    func08(&mut s);
-    println!("\t{}", s);
 }
 
 // one integer argument no return
@@ -39,41 +13,46 @@ fn test3() {
     func03(-1, 999);
 }
 
-fn test4() {}
-
-fn test5() {}
-
-fn test6() {}
-
-fn test7() {}
-
-fn test8() {}
-
-fn func08(s: &mut String) {
-    s.push_str(" from here");
+// two integer argument integer return
+fn test4() {
+    assert_eq!(func04(5, -3), 2);
 }
 
-fn func07(s: String) -> usize {
-    s.len()
+// two integer argument integer return with `return`
+fn test5() {
+    assert_eq!(func05(5, -3), 2);
 }
 
-fn func06(s: &str) -> usize {
-    s.len()
+// string reference argument
+fn test6() {
+    let s = String::from("hello world");
+
+    assert_eq!(func06(&s), s.len());
 }
 
-#[allow(clippy::needless_return)]
-fn func05(x: i8, y: i8) -> i8 {
-    println!("\targ1: {}", x);
-    println!("\targ2: {}", y);
+// string argument
+fn test7() {
+    let s = String::from("hello world");
+    let s_len = s.len();
 
-    return x + y; // look func04 for other possibility
+    assert_eq!(func07(s), s_len);
 }
 
-fn func04(x: i8, y: i8) -> i8 {
-    println!("\targ1: {}", x);
-    println!("\targ2: {}", y);
+// mutable string reference argument
+fn test8() {
+    let mut s = String::from("hello world");
 
-    x + y // look func05 for other possibility
+    func08(&mut s);
+
+    assert_eq!(s, "hello world from here");
+}
+
+fn func01() {
+    assert!(true);
+}
+
+fn func02(x: u8) {
+    assert_eq!(x, 3);
 }
 
 fn func03(x: i8, y: u16) {
@@ -81,12 +60,31 @@ fn func03(x: i8, y: u16) {
     assert_eq!(y, 999);
 }
 
-fn func02(x: u8) {
-    assert_eq!(x, 3);
+fn func04(x: i8, y: i8) -> i8 {
+    assert_eq!(x, 5);
+    assert_eq!(y, -3);
+
+    x + y
 }
 
-fn func01() {
-    assert!(true);
+#[allow(clippy::needless_return)]
+fn func05(x: i8, y: i8) -> i8 {
+    assert_eq!(x, 5);
+    assert_eq!(y, -3);
+
+    return x + y;
+}
+
+fn func06(s: &str) -> usize {
+    s.len()
+}
+
+fn func07(s: String) -> usize {
+    s.len()
+}
+
+fn func08(s: &mut String) {
+    s.push_str(" from here");
 }
 
 pub fn test() {
