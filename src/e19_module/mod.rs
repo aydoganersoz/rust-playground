c39c2d267fd7437fc95ec8e2fb6423f7b487c1cc
Aydogan Ersoz <aydoganersoz@protonmail.com>
2021-03-19 08:49:22 +0100

    refactor module (not finished)

--- src/e19_module/mod.rs
+++ src/e19_module/mod.rs
@@ -2,20 +2,24 @@ mod module_1;
 mod module_2;
 mod module_3;
 
+// module
 fn test1() {
-    println!("1. Module");
-    module_1::func1();
-    module_1::func2();
-    module_1::inner_module_1::func1();
-    module_1::inner_module_1::func2();
+    assert_eq!(module_1::func1(), 42);
+    assert_eq!(module_1::func2(), 65);
+    assert_eq!(module_1::inner_module_1::func1(), 17);
+    assert_eq!(module_1::inner_module_1::func2(), 23);
+}
 
+fn test2() {
     println!("2. Nested module");
     module_2::func1();
     module_2::func2();
     module_2::my_nested_module::func1();
     // module_2::func3(); // private function
     // module_2::my_nested_module::func2(); // private function
+}
 
+fn test3() {
     println!("3. Public struct - enum");
     module_3::func1();
     // following struct declaration is not allowed as it has a private field that does not permit to be accessed
@@ -38,4 +42,6 @@ fn test1() {
 
 pub fn test() {
     test1();
+    test2();
+    test3();
 }
--- src/e19_module/module_1.rs
+++ src/e19_module/module_1.rs
@@ -1,12 +1,15 @@
 pub mod inner_module_1;
 
-pub fn func1() {
-    println!("\tI am func1 in my_module_1");
+pub fn func1() -> u8 {
+    42
 }
 
-pub fn func2() {
-    func1();
-    println!("\tI am func2 in my_module_1");
-    inner_module_1::func2();
-    crate::e19_module::module_1::inner_module_1::func2(); // same as above
+pub fn func2() -> u8 {
+    let f1_res = func1();
+    let f2_res = inner_module_1::func2();
+    let f2_res_2 = crate::e19_module::module_1::inner_module_1::func2(); // same as above
+
+    assert_eq!(f2_res, f2_res_2);
+
+    f1_res + f2_res // 42 + 23
 }
