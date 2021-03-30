afb798b559aa00d9bf1a96e4acf02776daf0e794
Aydogan Ersoz <aydoganersoz@protonmail.com>
2021-03-30 08:37:58 +0200

    new match test

--- src/e17_match.rs
+++ src/e17_match.rs
@@ -10,6 +10,21 @@ fn test1() {
     assert_eq!(result, "losers");
 }
 
+fn test2() {
+    assert_eq!(is_leap_year(2000), true);
+    assert_eq!(is_leap_year(1999), false);
+}
+
+fn is_leap_year(year: u64) -> bool {
+    match (year % 4, year % 100, year % 400) {
+        (0, 0, 0) => true,
+        (0, 0, _) => false,
+        (0, _, _) => true,
+        _ => false,
+    }
+}
+
 pub fn test() {
     test1();
+    test2();
 }
